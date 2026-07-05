d46e7f893324ecbcc28639c39850969f09c3d5ef
Usman Ghani <usmanghanii7729@gmail.com>
2026-07-05 13:10:17 +0500

    Clean up repository: Remove old Django files and structure static frontend at root

--- PlantDiseaseDetection/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlantDiseaseDetection.settings')
-
-app = Celery('ppdd')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
--- PredictionModel/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-admin.site.register(Uploads)
-admin.site.register(UserImages)
-admin.site.register(User)
